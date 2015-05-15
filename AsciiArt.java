19ed1d13dc02f5755c566efabf1b59e897c4db80
Jordan Medlock <jordanemedlock@gmail.com>
2015-05-14 19:59:03 -0600

    Thursday May 14th

--- AsciiArt.java
+++ AsciiArt.java
@@ -1,38 +1,452 @@
 public class AsciiArt {
 	public static void main (String[] args){
+
+
 	letterT();
 	letterO();
 	letterM();
 }
 
 public static void letterT() {
-	System.out.println("_____________")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
+	System.out.println("_____________");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
 }
 
 public static void letterO() {
-	System.out.println(" ____________")
-	System.out.println("| __________ |")
-	System.out.println("|            |")
-	System.out.println("|            |")
-	System.out.println("|            |")
-	System.out.println("|            |")
-	System.out.println("| __________ |")
-	System.out.println("|____________|")
+	System.out.println(" ____________");
+	System.out.println("| __________ |");
+	System.out.println("|            |");
+	System.out.println("|            |");
+	System.out.println("|            |");
+	System.out.println("|            |");
+	System.out.println("| __________ |");
+	System.out.println("|____________|");
 }
 
 public static void letterM() {
-	System.out.println(" __        __")
-	System.out.println("|  |      |  |")
-	System.out.println("|   |    |   |")
-	System.out.println("|    |__|    |")
-	System.out.println("|            |")
-	System.out.println("|  ||    ||  |")
-	System.out.println("|__| |__| |__|")
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
+}
+public static void letterM() {
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
 }
 }
+
+/*
+System.out.println("....#....");
+System.out.println("...#.#...");
+System.out.println("..#####..");
+System.out.println(".#.....#.");
+System.out.println("#.......#");
+
+..#####..
+..#....#.
+..#####..
+..#....#.
+..#####..
+
+...####..
+..#......
+..#......
+..#......
+...####..
+
+D
+
+..####...
+..#...#..
+..#...#..
+..#...#..
+..####...
+
+E
+
+..#####..
+..#......
+..####...
+..#......
+..#####..
+
+F
+
+..#####..
+..#......
+..####...
+..#......
+..#......
+
+G
+
+...####..
+..#......
+..#..###.
+..#....#.
+...####..
+
+H
+
+..#....#.
+..#....#.
+..######.
+..#....#.
+..#....#.
+
+I
+
+.#######.
+....#....
+....#....
+....#....
+.#######.
+
+J
+
+.#######.
+.....#...
+.....#...
+.#...#...
+..###....
+
+K
+
+..#...#..
+..#..#...
+..###....
+..#..#...
+..#...#..
+
+L
+
+..#......
+..#......
+..#......
+..#......
+..######.
+
+M
+
+.#....#..
+.##..##..
+.#.##.#..
+.#....#..
+.#....#..
+
+N
+
+.#....#..
+.##...#..
+.#.#..#..
+.#..#.#..
+.#...##..
+
+O
+
+..####...
+.#....#..
+.#....#..
+.#....#..
+..####...
+
+P
+
+..####...
+.#....#..
+.#####...
+.#.......
+.#.......
+
+Q
+
+..####...
+.#....#..
+.#....#..
+.#..#.#..
+..####...
+
+R
+
+.#####...
+.#....#..
+.#####...
+.#...#...
+.#....#..
+
+S
+
+..#####..
+.#.......
+.######..
+......#..
+.#####...
+
+T
+
+.#######.
+....#....
+....#....
+....#....
+....#....
+
+U
+
+.#....#..
+.#....#..
+.#....#..
+.#....#..
+..####...
+
+V
+
+.#....#..
+.#....#..
+.#....#..
+..#..#...
+...##....
+
+W
+
+.#....#..
+.#....#..
+.#.##.#..
+.##..##..
+.#....#..
+
+X
+
+.#....#..
+..#..#...
+...##....
+..#..#...
+.#....#..
+
+Y
+
+.#....#..
+..#..#...
+....#....
+....#....
+....#....
+
+Z
+
+.######..
+.....#...
+....#....
+...#.....
+.######..
+
+*/
