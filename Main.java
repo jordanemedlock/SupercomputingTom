f498a9dba743c1b1710bed025e20372668682715
unknown <slappufoo@hotmail.com>
2015-05-14 17:11:53 -0700

    ascii art name

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.err.println("Hello World!");
+    System.err.println(args[0]);
+    System.err.println(args[1]);
+    System.err.println(args[2]);
+    try {
+	    Scanner s = new Scanner(new File("Main.java"));
+	    while (s.hasNext()){
+	    	System.err.println(s.nextLine());
+  		}
+  	} catch (Exception e) {
+
+  	}
   }
 }
