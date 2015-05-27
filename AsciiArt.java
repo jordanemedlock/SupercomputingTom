e5ee758b613149e5e96abde937a0cff0b6a38b60
unknown <slappufoo@hotmail.com>
2015-05-27 16:56:07 -0700

    all things defined, still dont understand parsing

--- AsciiArt.java
+++ AsciiArt.java
@@ -1,38 +1,41 @@
+import java.util.Scanner;
 public class AsciiArt {
 	public static void main (String[] args){
 	letterT();
 	letterO();
 	letterM();
+	Scanner scan = new Scanner(System.in);
+	String str = scan.nextLine();
 }
 
 public static void letterT() {
-	System.out.println("_____________")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
-	System.out.println("      |      ")
+	System.out.println("_____________");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
+	System.out.println("      |      ");
 }
 
 public static void letterO() {
-	System.out.println(" ____________")
-	System.out.println("| __________ |")
-	System.out.println("|            |")
-	System.out.println("|            |")
-	System.out.println("|            |")
-	System.out.println("|            |")
-	System.out.println("| __________ |")
-	System.out.println("|____________|")
+	System.out.println(" ____________");
+	System.out.println("| __________ |");
+	System.out.println("|            |");
+	System.out.println("|            |");
+	System.out.println("|            |");
+	System.out.println("|            |");
+	System.out.println("| __________ |");
+	System.out.println("|____________|");
 }
 
 public static void letterM() {
-	System.out.println(" __        __")
-	System.out.println("|  |      |  |")
-	System.out.println("|   |    |   |")
-	System.out.println("|    |__|    |")
-	System.out.println("|            |")
-	System.out.println("|  ||    ||  |")
-	System.out.println("|__| |__| |__|")
+	System.out.println(" __        __");
+	System.out.println("|  |      |  |");
+	System.out.println("|   |    |   |");
+	System.out.println("|    |__|    |");
+	System.out.println("|            |");
+	System.out.println("|  ||    ||  |");
+	System.out.println("|__| |__| |__|");
 }
 }
