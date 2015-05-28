c3613d123bf96b5763e3f2e1948da57de30c088d
unknown <slappufoo@hotmail.com>
2015-05-27 18:19:43 -0700

    Wednesday 27th

--- AsciiArt.java
+++ AsciiArt.java
@@ -1,41 +1,66 @@
 import java.util.Scanner;
 public class AsciiArt {
 	public static void main (String[] args){
-	letterT();
-	letterO();
-	letterM();
-	Scanner scan = new Scanner(System.in);
-	String str = scan.nextLine();
-}
+		letterT();
+		letterO();
+		letterM();
+		Scanner scan = new Scanner(System.in);
+		String str = "";
+		while (scan.hasNext() && !str.trim().equals("exit")) {
+			str = scan.nextLine();
+			for (int chars = 0; chars < str.length(); chars++){
 
-public static void letterT() {
-	System.out.println("_____________");
-	System.out.println("      |      ");
-	System.out.println("      |      ");
-	System.out.println("      |      ");
-	System.out.println("      |      ");
-	System.out.println("      |      ");
-	System.out.println("      |      ");
-}
+				char c = str.charAt(chars);
+				switch(c) {
+					case 't': 
+					case 'T': 
+					letterT();
+					break;
 
-public static void letterO() {
-	System.out.println(" ____________");
-	System.out.println("| __________ |");
-	System.out.println("|            |");
-	System.out.println("|            |");
-	System.out.println("|            |");
-	System.out.println("|            |");
-	System.out.println("| __________ |");
-	System.out.println("|____________|");
-}
+					case 'o':
+					case 'O': 
+					letterO();
+					break;
 
-public static void letterM() {
-	System.out.println(" __        __");
-	System.out.println("|  |      |  |");
-	System.out.println("|   |    |   |");
-	System.out.println("|    |__|    |");
-	System.out.println("|            |");
-	System.out.println("|  ||    ||  |");
-	System.out.println("|__| |__| |__|");
-}
+					case 'm':
+					case 'M': 
+					letterM();
+					break;
+
+				}
+			}
+		}
+	}
+
+
+	public static void letterT() {
+		System.out.println("_____________");
+		System.out.println("      |      ");
+		System.out.println("      |      ");
+		System.out.println("      |      ");
+		System.out.println("      |      ");
+		System.out.println("      |      ");
+		System.out.println("      |      ");
+	}
+
+	public static void letterO() {
+		System.out.println(" ____________");
+		System.out.println("| __________ |");
+		System.out.println("|            |");
+		System.out.println("|            |");
+		System.out.println("|            |");
+		System.out.println("|            |");
+		System.out.println("| __________ |");
+		System.out.println("|____________|");
+	}
+
+	public static void letterM() {
+		System.out.println(" __        __");
+		System.out.println("|  |      |  |");
+		System.out.println("|   |    |   |");
+		System.out.println("|    |__|    |");
+		System.out.println("|            |");
+		System.out.println("|  ||    ||  |");
+		System.out.println("|__| |__| |__|");
+	}
 }
