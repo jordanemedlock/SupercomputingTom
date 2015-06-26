c0fd61c86e56ba5603f3200b942f70b58b380654
unknown <slappufoo@hotmail.com>
2015-06-25 17:49:45 -0700

    many errors fixed lol

--- AsciiArt.java
+++ AsciiArt.java
@@ -1,47 +1,80 @@
+ import java.io.FileOutputStream;
+ import java.io.OutputStreamWriter;
+ import java.io.File;
+ import java.io.BufferedWriter;
+ import java.util.Scanner;
+ public class AsciiArt {
+ 	public static void main (String[] args){
+		File fout = new File("out.txt");
 
-import java.util.Scanner;
-public class AsciiArt {
-	public static void main (String[] args){
+ 		Scanner scan = new Scanner(System.in);
+ 		String str = "";
+ 		BufferedWriter bw = null;
+ 		try {
+			FileOutputStream fos = new FileOutputStream(fout);
 
-		Scanner scan = new Scanner(System.in);
-		String str = "";
-		while (scan.hasNext()) {
-			str = scan.nextLine();
-			if (str.equals("quit")) {
-				return;
-			} else {
+			bw = new BufferedWriter(new OutputStreamWriter(fos));
+		
+	 		while (scan.hasNext()) {
+	 			str = scan.nextLine();
+	 			if (str.equals("quit")) {
 
-			}
+	 				break;
+	 			} else {
+ 					bw.write(stringToAsciiArt(str));
+	 			}
+	 		}
+			bw.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-	}
-
-	public static String stringToAsciiArt(String s) {
-
-				for (int chars = 0; chars < s.length(); chars++){
-
-					char c = s.charAt(chars);
-					int n = charToNum (c);
-					String en = nthLetter(n);
+ 	}
 
 
+ 	public static String stringToAsciiArt(String s) {
 
+ 		String ret = "";
 
+ 		for (int chars = 0; chars < s.length(); chars++){
 
-					System.out.println(en);
+ 			char c = s.charAt(chars);
+ 			int n = charToNum (c);
+ 			String en = nthLetter(n);
+
+ 			ret += en;
+/*
+					//public static void writeFile1() throws IOException {
+						//File fout = new File("out.txt");
+						//FileOutputStream fos = new FileOutputStream(fout);
+						
+						BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+						
+						while (scan.hasNext()) {
+							bw.write(en);
+							bw.newLine();
+
+						}
+						
+						bw.close(); 
+					}
+*/
+					// System.out.println(en);
 				}
-	}
+				return ret;
+			}
 
-	public static int charToNum(char c) {
-		if (Character.isLowerCase(c)) {
-			return c - 97;
-		} else {
-			return c - 65;
+			public static int charToNum(char c) {
+				if (Character.isLowerCase(c)) {
+					return c - 97;
+				} else {
+					return c - 65;
+				}
+			}
+
+			public static String nthLetter(int n) {
+				return (letters).substring(n*50, n*50 +50); 
+			}
+
+			static String letters = "\n....#....\n...#.#...\n..#####..\n.#.....#.\n#.......#\n..#####..\n..#....#.\n..#####..\n..#....#.\n..#####..\n...####..\n..#......\n..#......\n..#......\n...####..\n..####...\n..#...#..\n..#...#..\n..#...#..\n..####...\n..#####..\n..#......\n..####...\n..#......\n..#####..\n..#####..\n..#......\n..####...\n..#......\n..#......\n...####..\n..#......\n..#..###.\n..#....#.\n...####..\n..#....#.\n..#....#.\n..######.\n..#....#.\n..#....#.\n.#######.\n....#....\n....#....\n....#....\n.#######.\n.#######.\n.....#...\n.....#...\n.#...#...\n..###....\n..#...#..\n..#..#...\n..###....\n..#..#...\n..#...#..\n..#......\n..#......\n..#......\n..#......\n..######.\n.#....#..\n.##..##..\n.#.##.#..\n.#....#..\n.#....#..\n.#....#..\n.##...#..\n.#.#..#..\n.#..#.#..\n.#...##..\n..####...\n.#....#..\n.#....#..\n.#....#..\n..####...\n..####...\n.#....#..\n.#####...\n.#.......\n.#.......\n..####...\n.#....#..\n.#....#..\n.#..#.#..\n..####...\n.#####...\n.#....#..\n.#####...\n.#...#...\n.#....#..\n..#####..\n.#.......\n.######..\n......#..\n.#####...\n.#######.\n....#....\n....#....\n....#....\n....#....\n.#....#..\n.#....#..\n.#....#..\n.#....#..\n..####...\n.#....#..\n.#....#..\n.#....#..\n..#..#...\n...##....\n.#....#..\n.#....#..\n.#.##.#..\n.##..##..\n.#....#..\n.#....#..\n..#..#...\n...##....\n..#..#...\n.#....#..\n.#....#..\n..#..#...\n....#....\n....#....\n....#....\n.######..\n.....#...\n....#....\n...#.....\n.######..";
+
 		}
-	}
-
-	public static String nthLetter(int n) {
-		return (letters).substring(n*50, n*50 +50); 
-	}
-
-	static String letters = "\n....#....\n...#.#...\n..#####..\n.#.....#.\n#.......#\n..#####..\n..#....#.\n..#####..\n..#....#.\n..#####..\n...####..\n..#......\n..#......\n..#......\n...####..\n..####...\n..#...#..\n..#...#..\n..#...#..\n..####...\n..#####..\n..#......\n..####...\n..#......\n..#####..\n..#####..\n..#......\n..####...\n..#......\n..#......\n...####..\n..#......\n..#..###.\n..#....#.\n...####..\n..#....#.\n..#....#.\n..######.\n..#....#.\n..#....#.\n.#######.\n....#....\n....#....\n....#....\n.#######.\n.#######.\n.....#...\n.....#...\n.#...#...\n..###....\n..#...#..\n..#..#...\n..###....\n..#..#...\n..#...#..\n..#......\n..#......\n..#......\n..#......\n..######.\n.#....#..\n.##..##..\n.#.##.#..\n.#....#..\n.#....#..\n.#....#..\n.##...#..\n.#.#..#..\n.#..#.#..\n.#...##..\n..####...\n.#....#..\n.#....#..\n.#....#..\n..####...\n..####...\n.#....#..\n.#####...\n.#.......\n.#.......\n..####...\n.#....#..\n.#....#..\n.#..#.#..\n..####...\n.#####...\n.#....#..\n.#####...\n.#...#...\n.#....#..\n..#####..\n.#.......\n.######..\n......#..\n.#####...\n.#######.\n....#....\n....#....\n....#....\n....#....\n.#....#..\n.#....#..\n.#....#..\n.#....#..\n..####...\n.#....#..\n.#....#..\n.#....#..\n..#..#...\n...##....\n.#....#..\n.#....#..\n.#.##.#..\n.##..##..\n.#....#..\n.#....#..\n..#..#...\n...##....\n..#..#...\n.#....#..\n.#....#..\n..#..#...\n....#....\n....#....\n....#....\n.######..\n.....#...\n....#....\n...#.....\n.######..";
-}
