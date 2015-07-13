bde5a9fc290e9fad43aefec272f17e4c08ed01d0
Jordan Medlock <jordanemedlock@gmail.com>
2015-07-13 16:55:05 -0600

    Fixed the path issue

--- Summer Games/HelloWorld.java
+++ Summer Games/HelloWorld.java
@@ -2,30 +2,30 @@ import org.lwjgl.Sys;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import java.util.*;
- 
+
 import java.nio.ByteBuffer;
- 
+
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.system.MemoryUtil.*;
- 
+
 public class HelloWorld {
- 
+
     // We need to strongly reference callback instances.
     private GLFWErrorCallback errorCallback;
     private GLFWKeyCallback   keyCallback;
- 
+
     // The window handle
     private long window;
- 
+
     public void run() {
         System.out.println("Hello LWJGL " + Sys.getVersion() + "!");
- 
+
         try {
             init();
             loop();
- 
+
             // Release window and window callbacks
             glfwDestroyWindow(window);
             keyCallback.release();
@@ -35,29 +35,29 @@ public class HelloWorld {
             errorCallback.release();
         }
     }
- 
+
     private void init() {
         // Setup an error callback. The default implementation
         // will print the error message in System.err.
         glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
- 
+
         // Initialize GLFW. Most GLFW functions will not work before doing this.
         if ( glfwInit() != GL11.GL_TRUE )
             throw new IllegalStateException("Unable to initialize GLFW");
- 
+
         // Configure our window
         glfwDefaultWindowHints(); // optional, the current window hints are already the default
         glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
         glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
- 
+
         int WIDTH = 300;
         int HEIGHT = 300;
- 
+
         // Create the window
         window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World!", NULL, NULL);
         if ( window == NULL )
             throw new RuntimeException("Failed to create the GLFW window");
- 
+
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
             @Override
@@ -66,7 +66,7 @@ public class HelloWorld {
                     glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
             }
         });
- 
+
         // Get the resolution of the primary monitor
         ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
         // Center our window
@@ -75,16 +75,16 @@ public class HelloWorld {
             (GLFWvidmode.width(vidmode) - WIDTH) / 2,
             (GLFWvidmode.height(vidmode) - HEIGHT) / 2
         );
- 
+
         // Make the OpenGL context current
         glfwMakeContextCurrent(window);
         // Enable v-sync
         glfwSwapInterval(1);
- 
+
         // Make the window visible
         glfwShowWindow(window);
     }
- 
+
     private void loop() {
         // This line is critical for LWJGL's interoperation with GLFW's
         // OpenGL context, or any context that is managed externally.
@@ -92,28 +92,28 @@ public class HelloWorld {
         // creates the ContextCapabilities instance and makes the OpenGL
         // bindings available for use.
         GLContext.createFromCurrent();
- 
+
         // Set the clear color
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
- 
+
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
         while ( glfwWindowShouldClose(window) == GL_FALSE ) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
- 
+
             glfwSwapBuffers(window); // swap the color buffers
- 
+
             // Poll for window events. The key callback above will only be
             // invoked during this call.
             glfwPollEvents();
         }
     }
- 
+
     public static void main(String[] args) {
-        Properties props = System.getProperties();
-        props.setProperty("java.library.path", "lwjgl/native/lwjgl32.dll");
+        // Properties props = System.getProperties();
+        // props.setProperty("java.library.path", "lwjgl/native/lwjgl32.dll");
 
         new HelloWorld().run();
     }
- 
+
 }
